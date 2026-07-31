9fe1accef129cc09ff96d5e5a3c3fbeb11802cd5
bakdauletkerimkulov <kerimkulovb@internet.ru>
2026-07-31 21:17:43 +0500

    added url strategy in tthe main.dart to hide # in url path

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:sarqyt/firebase_options.dart';
 import 'package:sarqyt/src/app_bootstrap.dart';
 import 'package:sarqyt/src/app_bootstrap_firebase.dart';
@@ -10,6 +12,8 @@ import 'package:sarqyt/src/features/notifications/domain/push_audience.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // turn off the # in the URLs on the web
+  usePathUrlStrategy();
 
   final appBootStrap = AppBootstrap();
   //! Uncomment this to connect to the Firebase emulators
